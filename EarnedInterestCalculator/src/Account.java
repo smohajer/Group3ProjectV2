b656c5c634bcbdf6e1c15eeb9b39d2ae36d54b6f
lcindydoan <lcindydoan@outlook.com>
2021-05-06 18:46:09 -0400

    Update Account.java

--- EarnedInterestCalculator/src/Account.java
+++ EarnedInterestCalculator/src/Account.java
@@ -1,6 +1,6 @@
 /*
  * filename: GUI.java
- * Authore: L. Cindy Doan
+ * Authors: L. Cindy Doan and Greg Fury
  * 05/01/2021
  * This application calculates the daily compounded interest earned from a checking or savings account balance.
  * Original source code found at https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
